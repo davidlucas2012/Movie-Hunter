436e4e5c0af796becb3e838fa87a50796629b1b9
David Lucas <dlucas@tastradesoft.com>
2021-02-09 01:31:17 +0800

    first commit

---
+++ Server/routes/posts.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { getMovie, createMovie } from "../controllers/posts.js";
+
+const router = express.Router();
+
+router.get("/", getMovie);
+
+router.post("/", createMovie);
+
+export default router;
